eab64a7ae39fa4a98d1c33d9f73a8a2a04b42a14
Faris <datiger365@gmail.com>
2023-05-27 16:40:35 -0400

    COMPLETE EARTH SIMULATION

--- src/main/java/project/sss/EarthSimulation.java
+++ src/main/java/project/sss/EarthSimulation.java
@@ -1,15 +1,30 @@
 /*
-CODE FOR FULL EARTH SIMULATION, STEP 1 OF THE SOLAR SYSTEM
+--------------------------------------------------
+SOLAR SYSTEM SIMULATION
+--------------------------------------------------
+
+IMPORTANT VALUES:
+
+CONVERSIONS:
+0.1 CHANGE PER FRAME = 1,037 MPH    (FOR ANIMATION TIMER)
+
+
+
+
+EARTH'S ROTATION SPEED: ~1,037 MPH
  */
 
 package project.sss;
 
+import Testing.ComplexShapes;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.*;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
@@ -26,8 +41,8 @@ import java.io.IOException;
 
 public class EarthSimulation extends Application {
 
-    private static final int WIDTH = 1280;
-    private static final int HEIGHT = 720;
+    private static final float WIDTH = 1400;
+    private static final float HEIGHT = 1000;
 
     private double anchorX, anchorY;
     private double anchorAngleX = 0;
@@ -35,136 +50,88 @@ public class EarthSimulation extends Application {
     private final DoubleProperty angleX = new SimpleDoubleProperty(0);
     private final DoubleProperty angleY = new SimpleDoubleProperty(0);
 
+    private final Sphere earth = new Sphere(150);
     @Override
     public void start(Stage stage) throws IOException {
+        Camera camera = new PerspectiveCamera(true);
+        camera.setNearClip(1);
+        camera.setFarClip(10000);
+        camera.translateZProperty().set(-1000);
+
+        SmartGroup system = new SmartGroup();
+        system.getChildren().add(prepareEarth());
+
+        Slider zoomSlider = zoomSlider();
+        system.translateZProperty().bind(zoomSlider.valueProperty());
+
+        // Seperate group for background objects (not mouse controllable)
+        Group root = new Group();
+        root.getChildren().add(system);
+        root.getChildren().add(prepareImages());
+        root.getChildren().add(zoomSlider);
 
 
-        Box box = prepareBox();
-
-        SmartGroup g = new SmartGroup();
-        g.getChildren().add(box);
-        g.getChildren().add(prepareBoxTwo());
-        g.getChildren().addAll(prepareLightSource());
-        g.getChildren().add(new AmbientLight());
-
-
-
-        g.translateXProperty().set(WIDTH/2);
-        g.translateYProperty().set(HEIGHT/2);
-        g.translateZProperty().set(-600);
-
-
-
-        // ******************************************
-
-
-        Camera camera = new PerspectiveCamera();
-        camera.translateZProperty().set(-200);
-
-
-        // ******************************************
-        Scene scene = new Scene(g, WIDTH, HEIGHT, true);
-
+        Scene scene = new Scene(root, WIDTH, HEIGHT, true);
         scene.setFill(Color.SILVER);
-        initMouseControl(g, scene, stage);
-        // Keyboard listener for moving sphere back and forward
-        stage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            switch (event.getCode()) {
-                case W: g.translateZProperty().set(g.getTranslateZ()  + 100);
-                    break;
-                case S: g.translateZProperty().set(g.getTranslateZ()  - 100);
-                    break;
-            }
-        });
+
+
+        // CAN MOVE THE SIMULATION AROUND
+        initMouseControl(system, scene, stage);
+
 
         scene.setCamera(camera);
-
         stage.setTitle("Solar System Simulation - By Faris Dababneh");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
 
-        // ANIMATION FOR ROTATION
+        prepareSpin();
+    }
+
+    private void prepareSpin() {
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                pointLight.setRotate(pointLight.getRotate() + 1);
+                earth.rotateProperty().set(earth.getRotate() + 0.1);
             }
         };
         timer.start();
     }
 
-    private Node prepareBoxTwo() {
-        PhongMaterial material = new PhongMaterial();
-        material.setDiffuseMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\Stylized_Stone_Floor_005_basecolor.jpg"))));
-        //material.setBumpMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\grunge.jpg"))));
+    private ImageView prepareImages() {
+        Image galaxy = new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\galaxy.jpg")));
+        ImageView galaxyView = new ImageView(galaxy);
+        galaxyView.setPreserveRatio(false);
 
-        Box box = new Box(100, 200, 100);
-        box.setMaterial(material);
-        return box;
+        galaxyView.getTransforms().add(new Translate(-galaxy.getWidth()/2, -galaxy.getHeight()/2, 6000));
+
+        return galaxyView;
     }
 
-    private final PointLight pointLight = new PointLight();
-    private Node[] prepareLightSource() {
+    private Node prepareEarth() {
+        PhongMaterial earthMaterial = new PhongMaterial();
+        earthMaterial.setDiffuseMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\earth-map.jpg"))));
+        earthMaterial.setSelfIlluminationMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\earth-illumination.jpg"))));
+        earthMaterial.setSpecularMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\earth-specular.tif"))));
+        earthMaterial.setBumpMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\earth-bump.tif"))));
 
-        // Ambient light gives everything the same light amount
-//        AmbientLight ambientLight = new AmbientLight();
-//        ambientLight.setColor(Color.AQUA);
-//        return ambientLight;
-
-        pointLight.setColor(Color.RED);
-        pointLight.getTransforms().add(new Translate(0, -200, 150));
-        // For the rotation animation
-        pointLight.setRotationAxis(Rotate.X_AXIS);
-
-        // TO REPRESENT THE POINT LIGHT
-        Sphere sphere = new Sphere(15);
-        // Position of point light
-        sphere.getTransforms().setAll(pointLight.getTransforms());
-        sphere.rotateProperty().bind(pointLight.rotateProperty());
-        sphere.rotationAxisProperty().bind(pointLight.rotationAxisProperty());
-
-
-        return new Node[]{pointLight, sphere};
+        earth.setRotationAxis(Rotate.Y_AXIS);
+        earth.setMaterial(earthMaterial);
+        return earth;
     }
 
-    private Box prepareBox() {
-        // Texture material object
-        PhongMaterial material = new PhongMaterial();
-        material.setDiffuseMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\Stylized_Stone_Floor_005_basecolor.jpg"))));
-        //material.setBumpMap(new Image(String.valueOf(new File("E:\\VSCODE PROJECTS\\Solar-System-Simulation\\src\\main\\java\\resources\\grunge.jpg"))));
+    private Slider zoomSlider() {
+        Slider zoom = new Slider();
+        zoom.setMax(800);
+        zoom.setMin(-200);
+        zoom.setPrefWidth(300d);
+        zoom.setLayoutX(-150);
+        zoom.setLayoutY(200);
+        zoom.setShowTickLabels(true);
+        zoom.setTranslateZ(5);
+        zoom.setStyle("-fx-base: black");
 
-        Box box = new Box(400, 80, 200);
-        box.setMaterial(material);
-        return box;
-    }
-
-    private void initMouseControl(SmartGroup group, Scene scene, Stage stage) {
-        Rotate xRotate;
-        Rotate yRotate;
-        group.getTransforms().addAll (
-                xRotate = new Rotate(0, Rotate.X_AXIS),
-                yRotate = new Rotate(0, Rotate.Y_AXIS)
-        );
-        xRotate.angleProperty().bind(angleX);
-        yRotate.angleProperty().bind(angleY);
-
-        scene.setOnMousePressed(event -> {
-            anchorX = event.getSceneX();
-            anchorY = event.getSceneY();
-            anchorAngleX = angleX.get();
-            anchorAngleY = angleY.get();
-        });
-
-        scene.setOnMouseDragged(event -> {
-            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
-            angleY.set(anchorAngleY + anchorX - event.getSceneX());
-        });
-
-        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
-            double delta = event.getDeltaY();
-            group.translateZProperty().set(group.getTranslateZ() + delta);
-        });
-
+        return zoom;
     }
 
     public static void main(String[] args) {
@@ -188,5 +155,39 @@ public class EarthSimulation extends Application {
             this.getTransforms().clear();
             this.getTransforms().addAll(t);
         }
+    }
+
+    private void initMouseControl(SmartGroup group, Scene scene, Stage stage) {
+        Rotate xRotate;
+        Rotate yRotate;
+        group.getTransforms().addAll (
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+        );
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+
+        scene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+
+
+        scene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY + anchorX - event.getSceneX());
+        });
+
+        /*
+       CODE FOR ZOOM CONTROL USING SCROLL WHEEL
+       REMOVED BECAUSE SLIDER IS USED FOR THIS PURPOSE
+
+        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
+            double delta = event.getDeltaY();
+            group.translateZProperty().set(group.getTranslateZ() + delta);
+        });*/
+
     }
 }
